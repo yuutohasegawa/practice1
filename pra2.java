2dd07228a231dda2c30b09513c37d4437331d7db
長谷川勇斗 <y.hasegawa.sys23@morijyobi.ac.jp>
2023-10-05 14:51:00 +0900

    aiueo

---
+++ pra2.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
